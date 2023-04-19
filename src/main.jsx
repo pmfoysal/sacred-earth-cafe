4249ad508b9a8443fafedc711e1597312a7e5251
pmfoysal <pmfoysal@gmail.com>
2023-04-19 11:24:41 +0600

    Some Necessary Packages Added

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import App from './app/app';
+import './styles/index.css';
+import store from './redux/store';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
    <React.StrictMode>
-      <App />
+      <Provider store={store}>
+         <BrowserRouter>
+            <HelmetProvider>
+               <App />
+            </HelmetProvider>
+         </BrowserRouter>
+      </Provider>
    </React.StrictMode>
 );
