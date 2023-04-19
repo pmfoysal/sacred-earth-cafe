fca66a7f0e7fdd42b67dd05bcda80b22f216328c
pmfoysal <pmfoysal@gmail.com>
2023-04-19 12:36:54 +0600

    App Routing Added

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,3 +1,12 @@
+import Home from '../pages/home';
+import Orders from '../pages/orders';
+import { Route, Routes } from 'react-router-dom';
+
 export default function App() {
-   return <div>App</div>;
+   return (
+      <Routes>
+         <Route path='/' element={<Home />} />
+         <Route path='/orders' element={<Orders />} />
+      </Routes>
+   );
 }
