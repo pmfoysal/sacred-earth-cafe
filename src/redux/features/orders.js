ce30dbd03a6e0ed9a0b54ae881ca7d8a1ee2b62e
pmfoysal <pmfoysal@gmail.com>
2023-04-19 16:33:44 +0600

    Orders Reducer Slice Updated

--- src/redux/features/orders.js
+++ src/redux/features/orders.js
@@ -12,11 +12,15 @@ const orders = createSlice({
    reducers: {
       addToCart: (state, action) => {
          const index = state.current.findIndex(order => order.id === action.payload.id);
+         // if product is not found in current orders
          if (index === -1) state.current.push(action.payload);
+         // if product is found in current orders
          else store.dispatch(orders.actions.updateQuantity({ id: action.payload.id, factor: 1 }));
       },
       updateQuantity: (state, action) => {
+         // find the product from current orders
          const selected = state.current.find(order => order.id === action.payload.id);
+         // if the product quantity is greater than 0 even after adding the factor
          if (selected.quantity + action.payload.factor > 0) {
             state.current = state.current.map(order => {
                if (order.id === action.payload.id) {
@@ -24,12 +28,17 @@ const orders = createSlice({
                }
                return order;
             });
-         } else state.current = state.current.filter(order => order.id !== action.payload.id);
+         }
+         // if the quantity of the product is not there after adding the factor
+         else state.current = state.current.filter(order => order.id !== action.payload.id);
       },
       confirmOrder: state => {
          state.current.forEach(cOrder => {
+            // find the index of each product from the previous orders
             const index = state.previous.findIndex(pOrder => pOrder.id === cOrder.id);
+            // if no product found from the previous orders
             if (index === -1) state.previous.push(cOrder);
+            // if the product is found from the previous orders
             else state.previous[index].quantity += cOrder.quantity;
          });
       },
